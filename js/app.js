f819bd370276b9111a9b0c293a56636c362cc78e
AngelEUrbinaN <ae.urbinanavarrete@ugto.mx>
2024-05-15 20:59:17 -0600

    Front básico para el login y registro

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const loginForm = document.getElementById('loginForm') || null 
+const registrarForm = document.getElementById('registrarForm') || null
+const signUpButton = document.getElementById('signUp') || null
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault() //para hacer refresh
+    const form = new FormData(loginForm)
+
+    fetch('../rentaAutos-back/index.php', {
+      method: 'POST',
+      body: form
+    })
+    .then((response) => response.json())
+    .then ((res) => {
+      console.log('@@ res =>', res)
+      if (res.message === 'Inicio Satisfactorio') {
+        console.log('Awebo funciono')
+    }
+    })
+    .catch((err) => {
+      console.log('@@ err =>', err)
+    }) 
+  })
+}
+
+if (registrarForm){
+  registrarForm.addEventListener('submit', (event) => {
+    event.preventDefault() //para hacer refresh
+    const form = new FormData(registrarForm)
+  
+    fetch('../rentaAutos-back/index.php', {
+      method: 'POST',
+      body: form
+    })
+    .then((response) => response.json())
+    .then ((res) => {
+      console.log('@@ res =>', res)
+      if (res.message === 'Usuario Registrado Satisfactoriamente') {
+        console.log('Qué chingón, se logró')
+    }
+    })
+    .catch((err) => {
+      console.log('@@ err =>', err)
+    })   
+  })
+}
+
+if (signUpButton) {
+  signUpButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.location.href = './registrar.html'
+  })
+}
