38e472235dbfbc481629cd29492c91ec684bc213
Coronilla <e.coronillahernandez@ugto.com>
2024-05-31 01:57:40 -0600

    Perfil enlazado a back

---
+++ js/perfil.js
@@ -0,0 +1,51 @@
+let idUser = document.getElementById('id')
+let nombreCompleto = document.getElementById('nombreCompleto')
+let nombreYApellidos = document.getElementById('nombreYApellidos')
+let fechaNacimiento = document.getElementById('fechaNacimiento')
+let genero = document.getElementById('genero')
+let correo = document.getElementById('correo')
+let telefono = document.getElementById('telefono')
+let direccion = document.getElementById('direccion')
+
+const actualizarForm = document.getElementById('actualizarForm') || null
+const userID = localStorage.getItem('userID')
+
+const idForm = document.getElementById('idFormUpdate')
+let inputId = document.getElementById('idUpdate')
+let inputAccion = document.getElementById('accion')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    if (userID) {
+        console.log('@@ id => ', userID)
+        obtenerDatosUsuario(userID)
+    }
+})
+
+const obtenerDatosUsuario = id => {
+    inputId.value = id
+    inputAccion.value = 'actualizar'
+    const form = new FormData(idForm)
+    
+    fetch('../rentaAutos-back/index.php', {
+        method: 'POST',
+        body: form
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('@@@ data => ', data)
+        if (data.success) {
+            idUser.value = data.user.usu_id
+            nombreCompleto.value = data.user.usu_nombre + " " + data.user.usu_apellidos
+            nombreYApellidos.value = data.user.usu_nombre + " " + data.user.usu_apellidos 
+            fechaNacimiento.value = data.user.usu_fechaNacimiento
+            genero.value = data.user.usu_genero
+            correo.value = data.user.usu_correo
+            telefono.value = data.user.usu_telefono
+            direccion.value = data.user.usu_direccion
+        }
+    })
+    .catch((err) => {
+        console.log('@@@ err => ', err)
+    })
+}
