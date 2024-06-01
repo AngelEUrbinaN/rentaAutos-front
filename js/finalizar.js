f6cf5336cecbac4227e88166ca9b731f623a7f09
AngelEUrbinaN <ae.urbinanavarrete@ugto.mx>
2024-06-01 12:40:43 -0600

    Sistema de pago y terminar renta finalizado

--- js/finalizar.js
+++ js/finalizar.js
@@ -1,6 +1,10 @@
 const userID = localStorage.getItem('userID')
 const finalizarForm = document.getElementById('finalizarForm') || null
 
+// Establecer los campos del auto y para finalizar la renta
+const idFormFinalizar = document.getElementById('idFormFinalizar')
+let idFinalizar = document.getElementById('id')
+let idAuto = document.getElementById('idAuto')
 let diaInicio = document.getElementById('diaInicio')
 let diaFin = document.getElementById('diaFin')
 let finReal = document.getElementById('finReal')
@@ -9,7 +13,7 @@ let costoEstimado = document.getElementById('costoEstimado')
 let costoReal = document.getElementById('costoReal')
 
 // Establecer campos para obtener el costo por dia
-const idFormFinalizar = document.getElementById('idFormFinalizar')
+const idFormRenta = document.getElementById('idFormRenta')
 let inputRentaId = document.getElementById('rentaID')
 let inputAccion = document.getElementById('accion')
 
@@ -32,10 +36,12 @@ const monto = document.getElementById('monto')
 const fecha = document.getElementById('fecha')
 const metodo = document.getElementById('metodo')
 
+// Funciones para obtener información desde el back //
+// Obtener información de la renta
 const obtenerAllData = id => {
     inputRentaId.value = id
     inputAccion.value = 'obtenerAllData'
-    const form = new FormData(idFormFinalizar)
+    const form = new FormData(idFormRenta)
 
     console.log('form id value =>', form.get('rentaID'))
     console.log('form accion value =>', form.get('accion'))
@@ -54,96 +60,13 @@ const obtenerAllData = id => {
       console.log('@@@ err => ', err)
       return null
     })
-  }
-
-document.addEventListener('DOMContentLoaded', async () => {
-  // Obtener parámetros del URL
-  const params = new URLSearchParams(window.location.search)
-  const rentaId = params.get('renta')
-
-  console.log(params)
-
-  if (userID) {
-      console.log('@@ idUser => ', userID)
-  }
-
-  if (rentaId) {
-    console.log('@@ idRenta => ', rentaId)
-    inputRentaId.value = rentaId
-    idRenta.value = rentaId
-
-    try {
-        const rentaData = await obtenerAllData(rentaId)
-        pintarRentaData(rentaData)
-        console.log(rentaData)
-        const autoData = await obtenerAutoData(rentaData.rent_aut_id)
-        pintarAutoData(autoData)
-        calcularCostoReal()
-      } catch (error) {
-        console.log('Error al obtener datos de la renta:', error)
-    }
-    
-  }
-})
-
-if (pagarForm){
-  pagarForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const form = new FormData(pagarForm)
-  
-    fetch('../rentaAutos-back/index.php', {
-      method: 'POST',
-      body: form
-    })
-    .then((response) => response.json())
-    .then ((res) => {
-      console.log('@@ res =>', res)
-      if (res.message === 'Pago Registrado Satisfactoriamente') {
-        console.log('Waos, se registro el pago')
-        // window.location.href = `../rentaAutos-front/home.html`
-    }
-    })
-    .catch((err) => {
-      console.log('@@ err =>', err)
-    }) 
-    
-    form = new FormData(finalizarForm)
-    
-      fetch('../rentaAutos-back/index.php', {
-        method: 'POST',
-        body: form
-      })
-      .then((response) => response.json())
-      .then ((res) => {
-        console.log('@@ res =>', res)
-        if (res.message === 'Renta Registrada Satisfactoriamente') {
-          console.log('Maracatanga, sí se registró')
-          window.location.href = `../rentaAutos-front/home.html`
-      }
-      })
-      .catch((err) => {
-        console.log('@@ err =>', err)
-      })   
-
-  })
 }
 
-const pintarRentaData = (rentaData) => {
-  diaInicio.value = rentaData.rent_diaInicio
-  diaFin.value = rentaData.rent_diaFin
-  costoEstimado.value = rentaData.rent_costoEstimado
-}
 
-const pintarAutoData = (auto) => {
-  asientos.value = auto.auto.aut_asientos
-  costoDiaNH.value = auto.auto.aut_costoDia
-  modelo.value = auto.auto.aut_modelo
-  localizacion.value = auto.auto.aut_localizacion
-  transmision.value = auto.auto.aut_transmision
-}
-
+// Obtiene todos los datos del auto
 const obtenerAutoData = id => {
   inputAutoId.value = id
+  idAuto.value = id
   inputAccionAuto.value = 'buscarAutoData'
   const form = new FormData(idFormAuto)
 
@@ -160,29 +83,105 @@ const obtenerAutoData = id => {
   })
 }
 
-if (finalizarForm){
-    finalizarForm.addEventListener('submit', (event) => {
-      event.preventDefault()
-      const form = new FormData(finalizarForm)
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // Obtener parámetros del URL
+  const params = new URLSearchParams(window.location.search)
+  const rentaId = params.get('renta')
+
+  console.log(params)
+
+  if (userID) {
+      console.log('@@ idUser => ', userID)
+  }
+
+  if (rentaId) {
+    console.log('@@ idRenta => ', rentaId)
+    inputRentaId.value = rentaId
+    idRenta.value = rentaId
+    idFinalizar.value = rentaId
+
+    try {
+        const rentaData = await obtenerAllData(rentaId)
+        pintarRentaData(rentaData)
+        console.log(rentaData)
+        const autoData = await obtenerAutoData(rentaData.rent_aut_id)
+        pintarAutoData(autoData)
+        calcularCostoReal()
+      } catch (error) {
+        console.log('Error al obtener datos de la renta:', error)
+    }
     
-      fetch('../rentaAutos-back/index.php', {
-        method: 'POST',
-        body: form
-      })
-      .then((response) => response.json())
-      .then ((res) => {
-        console.log('@@ res =>', res)
-        if (res.message === 'Renta Registrada Satisfactoriamente') {
-          console.log('Maracatanga, sí se registró')
-          window.location.href = `../rentaAutos-front/home.html`
-      }
-      })
-      .catch((err) => {
-        console.log('@@ err =>', err)
-      })   
+  }
+})
+
+// Funciones para mandar información al back //
+// Si se presiona el botón para pagar
+if (pagarForm){
+  pagarForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const form = new FormData(pagarForm)
+  
+    fetch('../rentaAutos-back/index.php', {
+      method: 'POST',
+      body: form
     })
+    .then((response) => response.json())
+    .then ((res) => {
+      console.log('@@ res =>', res)
+      if (res.message === 'Pago Registrado Satisfactoriamente') {
+        console.log('Waos, se registro el pago')
+        actualizarRenta()
+        // window.location.href = `../rentaAutos-front/home.html`
+    }
+    })
+    .catch((err) => {
+      console.log('@@ err =>', err)
+    }) 
+  })
 }
 
+// Es parte del botón para pagar la renta; actualiza los datos para
+  // agregar un fin a la renta y marcar el auto como disponible
+const actualizarRenta = () => {
+  const form = new FormData(finalizarForm)
+    
+  fetch('../rentaAutos-back/index.php', {
+    method: 'POST',
+    body: form
+  })
+  .then((response) => response.json())
+  .then ((res) => {
+    console.log('@@ res =>', res)
+    if (res.message === 'Renta Actualizada Satisfactoriamente') {
+      console.log('Waos, renta actualizada')
+      // window.location.href = `../rentaAutos-front/home.html`
+  }
+  })
+  .catch((err) => {
+    console.log('@@ err =>', err)
+  })   
+}
+
+// Funciones para mostrar los datos obtenidos en pantalla //
+// Coloca la información de la renta en los inputs
+const pintarRentaData = (rentaData) => {
+  diaInicio.value = rentaData.rent_diaInicio
+  diaFin.value = rentaData.rent_diaFin
+  costoEstimado.value = rentaData.rent_costoEstimado
+}
+
+// Coloca la información del auto en los inputs
+const pintarAutoData = (auto) => {
+  asientos.value = auto.auto.aut_asientos
+  costoDiaNH.value = auto.auto.aut_costoDia
+  modelo.value = auto.auto.aut_modelo
+  localizacion.value = auto.auto.aut_localizacion
+  transmision.value = auto.auto.aut_transmision
+}
+
+// Funciones matemáticas para mostrar en pantalla //
+// Calcula el costo finalizar el servicio hoy
 const calcularCostoReal = () => {
   const hoy = new Date()
   hoy.setHours(0, 0, 0, 0) // Establece la hora actual a medianoche
